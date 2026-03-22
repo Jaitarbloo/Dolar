9a273362831719bf211d99cf5f1a5824eebaf25b
Jaitarbloo <jaitarbloo@yahoo.es>
2026-03-22 11:24:54 +0100

    AÃ±adiendo mejoras y componentes

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,17 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Dolar",
+   
+    api_url="https://dolar-sed5.onrender.com",
+    
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://dolar-peach.vercel.app",
+        
+        ],
+
     plugins=[
+
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ]
